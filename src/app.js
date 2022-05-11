8be48f97ae1859a3dc1cf2317252547aae05a6be
Elikdev <koladeodutoye9913@gmail.com>
2022-05-11 09:40:44 +0100

    fix heroku deployment

--- src/app.js
+++ src/app.js
@@ -6,8 +6,12 @@ import { isCelebrateError } from "celebrate"
 import apiRoutes from "./routes.js"
 import { NODE_ENV } from "./config/index.js"
 import path from "path"
+import url from 'url';
 
 const app = express()
+const { fileURLToPath } = url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(morgan("dev"))
 app.use(cors())
@@ -26,14 +30,16 @@ if(NODE_ENV === 'production') {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
+} else {
+  app.get("*", (req, res) => {
+    return res.status(404).json({
+     status: false,
+     message: "Route not found"
+    })
+   })
 }
 
-app.get("*", (req, res) => {
- return res.status(404).json({
-  status: false,
-  message: "Route not found"
- })
-})
+
 
 app.use((error, req, res, next) => {
  if (isCelebrateError(error)) {
