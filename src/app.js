d0d7950b83e94c08c169ddcb092c236173950cf7
Elikdev <koladeodutoye9913@gmail.com>
2022-05-12 05:54:19 +0100

    fixing img not loading

--- src/app.js
+++ src/app.js
@@ -15,7 +15,14 @@ const __dirname = path.dirname(__filename);
 
 app.use(morgan("dev"))
 app.use(cors())
-app.use(helmet())
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"]
+    }
+  })
+)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
